166ab0dc8c7de0f6210e231872f976c0755378f3
Akshay Sharma <fxn769@gmail.com>
2020-12-07 15:05:46 +0530

    first work

---
+++ src/views/Sorting.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page {{type}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      type : this.$route.params.type
+    }
+  }, watch:{
+    '$route'(to,from){
+      this.type = this.$route.params.type
+    }
+  }
+}
+</script>
