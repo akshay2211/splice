d5003da7f03ed0c1ce586e1f83e6cc9230a9b403
Akshay Sharma <fxn769@gmail.com>
2020-12-13 16:49:16 +0530

    config updated

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     /* ... */
-    publicPath: process.env.NODE_ENV === 'production' ? '/dsa_algo/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/splice/' : '/'
   }
